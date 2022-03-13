839cb1306fb833f48bdb6b6fa2acdb89e45a99d4
Gakuto1112 <gakuto131112@gmail.com>
2022-03-13 13:30:41 +0900

    ゲーム前画面の作成

--- PictureJigsawPuzzle.js
+++ PictureJigsawPuzzle.js
@@ -9,7 +9,6 @@ puzzleImage.addEventListener("load", () => {
 	puzzleArea.style.height = "474px";
 	puzzleArea.classList.remove("puzzle_frame", "puzzle_area_empty");
 	puzzleImageElement.classList.add("puzzle_frame");
-	document.getElementById("puzzle_area").classList.remove("hidden");
 	if(imageRatio >= 16 / 9) {
 		puzzleImageElement.style.width = "800px"; 
 		puzzleImageElement.style.height = "";
@@ -22,6 +21,11 @@ puzzleImage.addEventListener("load", () => {
 		puzzleDivideCanvas.width = 450 * imageRatio;
 		puzzleDivideCanvas.height = 450;
 	}
+	document.getElementById("cannot_start_message").children.item(0).classList.add("hidden");
+	const column = document.getElementById("puzzle_column_division");
+	const row = document.getElementById("puzzle_row_division");
+	if(!column.classList.contains("text_box_invalid") && !row.classList.contains("text_box_invalid")) drawDivisionLine(column.value, row.value);
+	startCheck();
 });
 
 function selectImage() {
@@ -45,6 +49,86 @@ function selectImage() {
 	fileInput.click();
 }
 
+function divisionInputCheck() {
+	//分割数入力が正しいか検証
+	const column = document.getElementById("puzzle_column_division");
+	const row = document.getElementById("puzzle_row_division");
+	const pieceCount = document.getElementById("piece_count");
+	const start = document.getElementById("start");
+	const cannotStartMessage = document.getElementById("cannot_start_message");
+	startCheck();
+	if(column.value == 1 && row.value == 1) {
+		column.classList.add("text_box_invalid");
+		row.classList.add("text_box_invalid");
+		pieceCount.innerText = 1;
+		cannotStartMessage.children.item(1).classList.remove("hidden");
+		cannotStartMessage.children.item(2).classList.add("hidden");
+		cannotStartMessage.children.item(3).classList.add("hidden");
+		start.classList.add("button_disabled");
+	}
+	else {
+		cannotStartMessage.children.item(1).classList.add("hidden");
+		if(column.value < 1 || column.value % 1 != 0) {
+			column.classList.add("text_box_invalid");
+			cannotStartMessage.children.item(2).classList.remove("hidden");
+			start.classList.add("button_disabled");
+		}
+		else {
+			column.classList.remove("text_box_invalid");
+			cannotStartMessage.children.item(2).classList.add("hidden");
+			startCheck();
+		}
+		if(row.value < 1 || row.value % 1 != 0) {
+			row.classList.add("text_box_invalid");
+			cannotStartMessage.children.item(3).classList.remove("hidden");
+			start.classList.add("button_disabled");
+		}
+		else {
+			row.classList.remove("text_box_invalid");
+			cannotStartMessage.children.item(3).classList.add("hidden");
+			startCheck();
+		}
+	}
+	if(!column.classList.contains("text_box_invalid") && !row.classList.contains("text_box_invalid")) {
+		if(document.getElementById("puzzle_image").src != "") drawDivisionLine(column.value, row.value);
+		pieceCount.innerText = (column.value * row.value).toLocaleString();
+	}
+	else pieceCount.innerText = "??";
+}
+
+function drawDivisionLine(column, row) {
+	//プレビューの分割線を描画
+	const divisionLineCanvas = document.getElementById("puzzle_divide_canvas");
+	const canvasWidth = divisionLineCanvas.width;
+	const canvasHeight = divisionLineCanvas.height;
+	const context = divisionLineCanvas.getContext("2d");
+	context.clearRect(0, 0, 900, 450);
+	for(let i = 0; i < row; i++) {
+		for(let j = 0; j < column; j++) {
+			context.strokeStyle = "solid 1px dimgrey";
+			context.strokeRect(canvasWidth / column * j, canvasHeight / row * i, canvasWidth / column, canvasHeight / row);
+		}
+	}
+}
+
+function startCheck() {
+	//スタートできるかチェックし、ボタンの有効化/無効化
+	if(!Array.prototype.slice.call(document.getElementById("cannot_start_message").children).find((message) => !message.classList.contains("hidden"))) {
+		document.getElementById("start").classList.remove("button_disabled");
+	}
+}
+
+//以下main
+//ブラウザがキャンバス描画に対応している確認
+const canvasForCheck = document.createElement("CANVAS");
+if(canvasForCheck.getContext) document.getElementById("message_canvas_not_available").remove();
+else {
+	document.getElementById("puzzle_area").remove();
+	document.getElementById("puzzle_division_settings").remove();
+}
+delete canvasForCheck;
+
+//背景のアニメーション
 setInterval(() => {
 	if(document.body.style.backgroundPosition == "") document.body.style.backgroundPosition = "0px 0px";
 	else {
