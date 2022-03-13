f9d45e96825d128f352bb1382aca7eb82b8e39a2
Gakuto1112 <gakuto131112@gmail.com>
2022-03-13 16:17:22 +0900

    画像がバラバラに分解されるところまで

--- PictureJigsawPuzzle.js
+++ PictureJigsawPuzzle.js
@@ -8,7 +8,8 @@ puzzleImage.addEventListener("load", () => {
 	puzzleArea.style.width = "824px";
 	puzzleArea.style.height = "474px";
 	puzzleArea.classList.remove("puzzle_frame", "puzzle_area_empty");
-	puzzleImageElement.classList.add("puzzle_frame");
+	puzzleDivideCanvas.classList.add("puzzle_frame");
+	document.getElementById("puzzle_empty_text").classList.add("hidden");
 	if(imageRatio >= 16 / 9) {
 		puzzleImageElement.style.width = "800px"; 
 		puzzleImageElement.style.height = "";
@@ -45,24 +46,26 @@ function fadeOutElement(element, transition) {
 	}, { once: true });
 }
 
-function selectImage() {
+function selectImage(clickElement) {
 	//画像選択ウィンドウを開けて、画像を選択し、画像を表示させる処理
-	const fileInput = document.createElement("INPUT");
-	fileInput.type = "file";
-	fileInput.accept = "image/*";
-	fileInput.addEventListener("change", (event) => {
-		const acceptFileType = ["png", "jpg", "jpeg"];
-		if(acceptFileType.indexOf(fileInput.value.split(".").slice(-1)[0]) >= 0) {
-			//ファイルの形式が正しい場合の処理
-			const reader = new FileReader();
-			reader.addEventListener("load", (event) => {
-				puzzleImage.src = event.target.result;
-			});
-			reader.readAsDataURL(event.target.files[0]);
-		}
-		else alert("選択したファイルの拡張子が正しくありません。\n使用できる拡張子は " + acceptFileType.join(", ") + " です。");
-	});
-	fileInput.click();
+	if(clickElement.classList.contains("puzzle_area_image_select")) {
+		const fileInput = document.createElement("INPUT");
+		fileInput.type = "file";
+		fileInput.accept = "image/*";
+		fileInput.addEventListener("change", (event) => {
+			const acceptFileType = ["png", "jpg", "jpeg"];
+			if(acceptFileType.indexOf(fileInput.value.split(".").slice(-1)[0]) >= 0) {
+				//ファイルの形式が正しい場合の処理
+				const reader = new FileReader();
+				reader.addEventListener("load", (event) => {
+					puzzleImage.src = event.target.result;
+				});
+				reader.readAsDataURL(event.target.files[0]);
+			}
+			else alert("選択したファイルの拡張子が正しくありません。\n使用できる拡張子は " + acceptFileType.join(", ") + " です。");
+		});
+		fileInput.click();
+	}
 }
 
 function divisionInputCheck() {
@@ -134,18 +137,51 @@ function startCheck() {
 	}
 }
 
-function start(buttonElement) {
+function start(clickElement) {
 	//ゲーム開始
-	if(!buttonElement.classList.contains("button_disabled")) {
+	if(!clickElement.classList.contains("button_disabled")) {
 		document.getElementById("puzzle_column_division").disabled = true;
 		document.getElementById("puzzle_row_division").disabled = true;
-		buttonElement.classList.add("button_disabled");
+		clickElement.classList.add("button_disabled");
+		document.getElementById("puzzle_area").classList.remove("puzzle_area_image_select");
 		swapClass(document.body, "background_blue", "background_green");
 		swapClass(document.getElementById("header"), "header_blue", "header_green");
 		fadeOutElement(document.getElementById("puzzle_division_settings"), 1.5);
 		fadeOutElement(document.getElementById("puzzle_piece_count"), 1.5);
-		fadeOutElement(document.getElementById("start"), 1.5);
+		fadeOutElement(clickElement, 1.5);
 		fadeOutElement(document.getElementById("cannot_start_message"), 1.5);
+		clickElement.addEventListener("transitionend", () => {
+			//画像の分割
+			const puzzleImageCanvas = document.getElementById("puzzle_divide_canvas");
+			const column = document.getElementById("puzzle_column_division").value;
+			const row = document.getElementById("puzzle_row_division").value;	
+			const puzzlePieceArea = document.getElementById("puzzle_piece_area");
+			let puzzlePieceFadeoutCount = 0;
+			puzzlePieceArea.style.gridTemplateColumns = "repeat(" + column + ", 1fr)";
+			puzzlePieceArea.style.gridTemplateRows = "repeat(" + row + ", 1fr)";
+			document.getElementById("puzzle_area").classList.add("puzzle_area_empty");
+			for(let i = 0; i < row; i++) {
+				for(let j = 0; j < column; j++) {
+					const puzzlePiece = document.createElement("CANVAS");
+					const context = puzzlePiece.getContext("2d");
+					puzzlePiece.setAttribute("data-piece-column", j);
+					puzzlePiece.setAttribute("data-piece-row", i);
+					puzzlePiece.width = puzzleImageCanvas.width / column;
+					puzzlePiece.height = puzzleImageCanvas.height / row;
+					context.drawImage(puzzleImage, puzzleImage.naturalWidth / column * j, puzzleImage.naturalHeight / row * i, puzzleImage.naturalWidth / column, puzzleImage.naturalHeight / row, 0, 0, puzzleImageCanvas.width / column, puzzleImageCanvas.height / row);
+					puzzlePieceArea.appendChild(puzzlePiece);
+				}
+			}
+			document.getElementById("puzzle_image").classList.add("hidden");
+			const puzzlePieceFadeoutInterval = setInterval(() => {
+				puzzlePieceArea.children.item(puzzlePieceFadeoutCount).classList.add("puzzle_piece_fadeout");
+				puzzlePieceArea.children.item(puzzlePieceFadeoutCount).addEventListener("animationend", (event) => event.target.style.opacity = 0, { once: true });
+				if(puzzlePieceFadeoutCount < column * row -1) {
+					puzzlePieceFadeoutCount++;
+				}
+				else clearInterval(puzzlePieceFadeoutInterval);
+			}, 3000 / (column * row));
+		}, { once: true });
 	}
 }
 
